e1f068d01fe7fdf1196bccde51b209486397b2b7
Morrigan Mahady <morri.mahady@gmail.com>
2025-07-28 20:49:46 -0500

    Set locale to US for number configuration

--- services/display_utils.py
+++ services/display_utils.py
@@ -32,7 +32,7 @@ coco = country_converter.CountryConverter()
 
 # Set system locale for number formatting (fallback to default)
 try:
-    locale.setlocale(locale.LC_ALL, "")
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 except Exception:
     locale.setlocale(locale.LC_ALL, "C")
 
